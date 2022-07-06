308b32ac9f0ba5ce317604c641794dc6c46025fe
HyeonsooKim <hyeonsoo.kim@codestates.com>
2022-07-07 00:57:21 +0900

    Devide base.html with header and footer. Use block content. Use render function

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
