4b8e532915b47461fcff7220fccf4aacb932d94c
HyeonsooKim <hyeonsoo.kim@codestates.com>
2022-07-08 01:55:19 +0900

    Use for loop for DB data. Redirect in app.

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from accountapp.views import hello_world
 
-app_name = 'account_app'
+app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world')
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from accountapp.models import HelloWorld
 # Create your views here.
@@ -13,7 +14,8 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
