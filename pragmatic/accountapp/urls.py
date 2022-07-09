dce2a0908f43289bc1e96ebfaa0c8336625880b0
HyeonsooKim <hyeonsoo.kim@codestates.com>
2022-07-09 22:32:50 +0900

    Add create account class.

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path, include
 
-from accountapp.views import hello_world
+from accountapp.views import AccountCreateView, hello_world
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.urls import reverse
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
-# Create your views here.
 
 def hello_world(request):
     if request.method == "POST":
@@ -19,3 +23,9 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
