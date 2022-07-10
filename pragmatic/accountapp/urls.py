bca98ef03eb7d39565a7c806c0a50c8681537c77
HyeonsooKim <hyeonsoo.kim@codestates.com>
2022-07-11 03:31:58 +0900

    Add DeleteView

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -2,7 +2,7 @@ from re import template
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, hello_world
+from accountapp.views import AccountCreateView, AccountDeleteView, AccountDetailView, AccountUpdateView, hello_world
 
 app_name = 'accountapp'
 
@@ -15,5 +15,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
-
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
